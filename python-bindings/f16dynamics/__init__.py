03a63989634fedb6da9dab4e29977a3f41ced228
Ethan Lew <elew@galois.com>
2022-04-25 15:59:15 -0700

    CSAF F16 wrappers in python module

---
+++ python-bindings/f16dynamics/__init__.py
@@ -0,0 +1,9 @@
+from f16dynamics.f16dynpy import *
+
+__author__ = ["Ethan Lew"]
+__credits__ = ["Ethan Lew"]
+__license__ = "GPLv3"
+__version__ = "0.1"
+__maintainer__ = "Ethan Lew"
+__email__ = "ethanlew16@gmail.com"
+__status__ = "prototype"
---
+++ python-bindings/f16dynamics/csaf.py
@@ -0,0 +1,39 @@
+from csaf_f16.systems import F16Simple
+from csaf_f16.messages import *
+from csaf_f16.components import F16PlantComponent
+
+from f16dynamics.f16dynpy import F16Plant
+import numpy as np
+
+# create context for the optimized model
+f16 = F16Plant()
+
+def model_state_update(model, time_t, state_f16, input_controller):
+    """the new optimized state evolution function"""
+    s = np.array(state_f16)
+    u = np.array(input_controller)
+    return f16.dxdt(s, u)[:13]
+
+
+def model_output(model, time_t, state_f16, input_controller):
+    """the new optimized output function"""
+    s = np.array(state_f16)
+    u = np.array(input_controller)
+    return f16.dxdt(s, u)[13:]
+
+
+class F16FastPlantComponent(F16PlantComponent):
+    """wrap the optimized methods into a CSAF component"""
+    flows = {
+        "outputs": model_output,
+        "states": model_state_update
+    }
+
+
+class F16FastSimple(F16Simple):
+    """place the component into a simple controls architecture"""
+    components = {
+        **F16Simple.components,
+        "plant": F16FastPlantComponent,
+
+    }
